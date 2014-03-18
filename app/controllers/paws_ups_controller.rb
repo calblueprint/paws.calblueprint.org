f858778aac145cf92e64cc15137e5df73266ea10
Mark Miyashita <negativetwelve@gmail.com>
2014-03-17 17:29:37 -0700

    Add create method for paws up and start the form on the home page

--- app/controllers/paws_ups_controller.rb
+++ app/controllers/paws_ups_controller.rb
@@ -1,2 +1,12 @@
 class PawsUpsController < ApplicationController
+
+  def create
+    @paws_up = PawsUp.new(paws_up_params)
+    if @paws_up.save
+      redirect_to root_path, success: "Successfully sent Paws Up!"
+    else
+      redirect_to root_path, error: "There was a problem sending your Paws Up, please try again."
+    end
+  end
+
 end
--- app/models/paws_up.rb
+++ app/models/paws_up.rb
@@ -1,2 +1,3 @@
 class PawsUp < ActiveRecord::Base
+  validates :description, presence: true
 end
