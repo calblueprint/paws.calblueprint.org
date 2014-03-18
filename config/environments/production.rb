e2ebff0354910deb11eadd93c335ff2c3d3dc13c
Mark Miyashita <negativetwelve@gmail.com>
2014-03-18 01:20:15 -0700

    Add google analytics tracker id

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@ PawsUpBlueprint::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  GA.tracker = "UA-47310897-3"
 end
