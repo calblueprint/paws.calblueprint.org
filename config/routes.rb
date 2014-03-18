5b036ab678d7434ec16b729ab94bd5f729e9cd52
Mark Miyashita <negativetwelve@gmail.com>
2014-03-18 00:01:52 -0700

    Add routes for paws_up

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ PawsUpBlueprint::Application.routes.draw do
   root to: "pages#home"
 
   match "about", to: "pages#about", via: :get
+  match "/", to: "paws_up#create", as: "paws_ups", via: :post
 end
