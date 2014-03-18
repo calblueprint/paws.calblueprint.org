3fd7958ca5597a07917f493a741303475dfd5238
Mark Miyashita <negativetwelve@gmail.com>
2014-03-18 00:29:30 -0700

    Style the form and add seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+bloopers = [
+  "Melissa Huang",
+  "Mark Miyashita",
+  "Alice Oh",
+  "Jessica Lin",
+  "Wonjun Jeong",
+  "Kevin Gong",
+  "Tony Wu",
+  "Atsu Kakitani",
+  "Sam Lau",
+  "Hao Guan",
+  "Nishant Desai",
+  "Alli Leong",
+  "Ethan Chiou",
+  "Will Tang",
+  "Varun Rau",
+  "Anthony Sutardja",
+  "Vivek Raghuram",
+  "John Du",
+  "Eugene Kim",
+  "Jonathan Ho",
+  "Byron Zhang",
+  "Howard Chen",
+  "Vincent Tian",
+  "Alton Zheng-Xie",
+  "Brian Liou",
+  "Deeps Warrier",
+  "Kevin Wu",
+  "Justin Chan",
+  "Jennifer Min",
+]
+
+bloopers.each do |blooper|
+  user = User.where(name: blooper).first_or_create!
+  if user.new_record?
+    puts "Created: #{user.name}."
+  end
+end
