f40616a3d0a97cecfb1e24ae178d3da187b2ac54
Mark Miyashita <negativetwelve@gmail.com>
2014-03-18 00:02:17 -0700

    Create users, migrate db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140318001836) do
+ActiveRecord::Schema.define(version: 20140318070100) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 20140318001836) do
     t.integer  "user_id"
     t.integer  "from_user_id"
     t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
