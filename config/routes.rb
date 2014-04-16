a495c6b921f7d276eb527763bd72d2466fd69c76
Mark Miyashita <negativetwelve@gmail.com>
2014-04-15 17:46:41 -0700

    Add post route for viewing secret page

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ PawsUpBlueprint::Application.routes.draw do
   root to: "pages#home"
 
   match "/", to: "paws_up#create", as: "paws_ups", via: :post
-  match "view", to: "paws_up#index", via: :get
+  match "secret", to: "paws_up#index", via: :get
+  match "secret", to: "paws_up#index", via: :post
 end
