75c924f4ad5f334d9c5d1e91521b5688a61402cb
Sam Lau <samlau95@gmail.com>
2015-01-13 01:47:15 -0800

    Use to_s for easier paws up rendering

--- app/models/paws_up.rb
+++ app/models/paws_up.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: paws_ups
+#
+#  id           :integer          not null, primary key
+#  user_id      :integer
+#  from_user_id :integer
+#  description  :text
+#  created_at   :datetime
+#  updated_at   :datetime
+#  custom_for   :string
+#  value        :string
+#
+
 class PawsUp < ActiveRecord::Base
   validates :description, presence: true
   validate :has_user_id_or_custom_for
@@ -16,4 +30,17 @@ class PawsUp < ActiveRecord::Base
     end
   end
 
+  def to_s
+    "Paws Up to #{recipient} for #{description}" + value_string
+  end
+
+  private
+
+  def value_string
+    if value
+      " (#{value})"
+    else
+      ""
+    end
+  end
 end
