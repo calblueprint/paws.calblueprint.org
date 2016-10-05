fc85541fb8db50d51336dfe862be11f41595039b
vivekraghuram <VV9295@YAHOO.COM>
2016-10-04 20:54:25 -0700

    Update members file and keep track of inactive users and filter them out

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,15 +2,21 @@ require "open-uri"
 
 # Most accurate record of current Blueprint members
 MEMBERS_FILE =
-  "https://raw.githubusercontent.com/calblueprint/calblueprint.org/master/data/members.yml"
+  "https://raw.githubusercontent.com/calblueprint/calblueprint.org/master/config/locales/en/members.yml"
 
-bloopers = YAML.parse(open(MEMBERS_FILE)).to_ruby['members']
+bloopers = YAML.parse(open(MEMBERS_FILE)).to_ruby['en']['members']
+
+User.all.each do |u|
+  u.update_attributes!(inactive: true)
+end
 
 bloopers.each do |blooper|
-  user = User.where(name: "#{blooper['first_name']} #{blooper['last_name']}").first_or_create!
+  user = User.find_or_initialize_by(name: "#{blooper['first_name']} #{blooper['last_name']}")
   unless user.persisted?
+    user.save!
     puts "Created: #{user.name}."
   else
+    user.update_attributes!(inactive: false)
     puts "Already had user: #{user.name}."
   end
 end
