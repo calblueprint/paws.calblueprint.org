1ce6bb34289890c8635db6434e8081b23effee92
aivantg <aivant.developer@gmail.com>
2019-03-09 20:23:23 -0800

    randomized paws up

--- app/controllers/paws_up_controller.rb
+++ app/controllers/paws_up_controller.rb
@@ -18,17 +18,8 @@ class PawsUpController < ApplicationController
 
   def index
     if params[:secret].nil? || params[:secret] == ENV["secret"]
-      @current_paws_up = PawsUp.where(cycle: Cycle.last).order(created_at: :desc)
+      @current_paws_up = PawsUp.where(cycle: Cycle.last).order(created_at: :desc).shuffle
       @previous_paws_up = PawsUp.where.not(cycle: Cycle.last).order(created_at: :desc)
-
-      if params[:random] == "true"
-        @current_paws_up = @current_paws_up.shuffle
-        @random_text = "Chronological Order"
-        @new_path = "/secret?random=false"
-      else
-        @random_text = "Random Order"
-        @new_path = "/secret?random=true"
-      end
     else
       flash[:danger] = "You are not allowed to view this page."
       redirect_to root_path
