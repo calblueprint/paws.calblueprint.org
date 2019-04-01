f3fa5813d795d03e5e990add39981ed95e24b419
aivantg <aivant.developer@gmail.com>
2019-03-31 21:46:34 -0700

    fixed neglect and added parameter for weeks

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,9 +8,14 @@ class PagesController < ApplicationController
   end
 
   def neglect
-    cycle_ids = Cycle.all.reverse_order[1, 3].map {|c| c.id} 
-    recipients = PawsUp.where(id: cycle_ids).map { |p| p.user_id}
-    @neglected_bloopers = User.where.not(inactive: true, id: recipients)
+    if params[:weeks].nil? 
+      redirect_to '/neglected?weeks=2'
+    else
+      @weeks = [params[:weeks].to_i, Cycle.count - 1].min
+      cycle_ids = Cycle.all.reverse_order[0, @weeks].map {|c| c.id} 
+      recipients = PawsUp.where(cycle_id: cycle_ids).map { |p| p.user_id}
+      @neglected_bloopers = User.where.not(inactive: true, id: recipients)
+    end
   end
 
 end
